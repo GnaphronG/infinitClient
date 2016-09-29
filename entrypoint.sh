c97e09a4da5dec816fae1d1562fe06faa024b66f
Guillaume Goussard <Guillaume.Goussard@mgo.com>
2016-09-28 22:44:23 -0700

    Add entrypoint script and Dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+mkdir -p /mnt/shared
+
+infinit-network --fetch --as $INFINI_USER
+infinit-volume --fetch --as $INFINIT_USER
+
+infinit-network --link --as $INFINIT_USER --name $INFIT_NETWORK_CLUSTER
+
+infinit-volume --mount --as $INFINIT_USER --name $INFINIT_VOLUME_NAME --async --cache --publish --mountpoint /mnt/shared/
